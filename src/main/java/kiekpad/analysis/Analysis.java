7647078fafd22a767dd4ee633081550b40a17555
Sören Henning <stu114708@informatik.uni-kiel.de>
2016-07-22 11:52:07 +0200

    added example configuration

--- src/main/java/kiekpad/analysis/Analysis.java
+++ src/main/java/kiekpad/analysis/Analysis.java
@@ -1,13 +1,40 @@
 package kiekpad.analysis;
 
+import java.time.Duration;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+import kiekpad.analysis.domain.RecordFilter;
+import teead.aggregation.MeanAggregator;
+import teead.forecast.RegressionForecaster;
+import teead.storage.CassandraDriver;
 import teetime.framework.Execution;
 
 public class Analysis {
 
 	public static void main(final String[] args) {
-		AnalysisConfiguration analysisConfiguration = new AnalysisConfiguration();
+		executeAnalysis(buildExampleConfiguration());
+	}
+
+	public static void executeAnalysis(final AnalysisConfiguration analysisConfiguration) {
 		final Execution<AnalysisConfiguration> analysis = new Execution<>(analysisConfiguration);
 		analysis.executeBlocking();
+	}
+
+	private static AnalysisConfiguration buildExampleConfiguration() {
+		String ipAddress = "192.168.99.100";
+		int port = 32770;
+		String keyspace = "demo3";
+
+		Cluster cluster = Cluster.builder().addContactPoint(ipAddress).withPort(port).build();
+		Session session = cluster.connect(keyspace);
+
+		AnalysisConfiguration analysisConfiguration = new AnalysisConfiguration();
+		analysisConfiguration.addAnalysis(new RecordFilter(), Duration.ofHours(1), Duration.ofSeconds(5), new MeanAggregator(), new RegressionForecaster(),
+				new CassandraDriver(session, "measurements", "example-name"));
+
+		return analysisConfiguration;
 	}
 
 }
